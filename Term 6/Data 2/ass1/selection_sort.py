dfcaf2c16b6393592907e5891c460c4ac962c62e
Mustafa Ahmed Elsayed <ma762955@gmail.com>
2025-02-28 03:09:24 +0200

    Update selection_sort.py
    tm wd3 el code fe mkaneh alsleem brga2 morag3th 2bl aste5damoh

--- Term 6/Data 2/ass1/selection_sort.py
+++ Term 6/Data 2/ass1/selection_sort.py
@@ -1,2 +1,29 @@
-def selection_sort(arr):
-    pass
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+def selection_sort(array):
+    for i in range(len(array)):
+        j=i
+        while j>0 and array[j-1]>array[j]:
+            array[j], array[j-1] = array[j-1], array[j]
+            j -= 1
+    return array
+
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key
+    return array
+
+if __name__ == '__main__':
+    print(bubble_sort([7,6,2,-3,8,4,2,0,6]))
+    print(selection_sort([7,6,2,-3,8,4,2,0,6]))
+    print(insertion_sort([7,6,2,-3,8,4,2,0,6]))
